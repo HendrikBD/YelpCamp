825002bdf9dab3c70c3a378c6ad402e181cdfc85
HendrikBD <hendrik.danen@gmail.com>
2018-02-27 16:18:13 -0500

    Condensing code & updating scroll height when window size changes

--- public/scripts/show.js
+++ public/scripts/show.js
@@ -1,34 +1,33 @@
 var navBtns = document.querySelector(".navBtns");
 var listItems = document.querySelectorAll(".list-group-item");
 
-listItems[0].addEventListener("click", function(){
-  listItems.forEach(function(item){
-    item.classList.remove("act")
-  })
-  this.classList.add("act")
-  var jump = document.getElementById("activities").offsetTop;
-  window.scrollTo(0, jump+70-window.innerHeight);
-})
 
-listItems[1].addEventListener("click", function(){
-  listItems.forEach(function(item){
-    item.classList.remove("act")
-  })
-  this.classList.add("act")
-  var jump = document.getElementById("comments").offsetTop;
-  window.scrollTo(0, jump+70-window.innerHeight);
-})
+setNavBtns();
 
-listItems[2].addEventListener("click", function(){
-  listItems.forEach(function(item){
-    item.classList.remove("act")
-  })
-  this.classList.add("act")
-  var jump = document.getElementById("comments").offsetTop;
-  window.scrollTo(0, jump+70);
-})
+window.onresize = function(event){
+  setNavBtns();
+}
+
+
+function setNavBtns(){
+  jumpLocs = [
+    document.getElementById("activities").offsetTop + 70 - window.innerHeight,
+    document.getElementById("comments").offsetTop + 70 - window.innerHeight,
+    document.getElementById("comments").offsetTop + 70
+  ];
+
+  for(var i=0; i<listItems.length; i++){
+    listItems[i].jumpLoc = jumpLocs[i];
+    listItems[i].addEventListener("click", function(){
+      listItems.forEach(function(item){ item.classList.remove("act") })
+      this.classList.add("act");
+      window.scroll(0, this.jumpLoc);
+    })
+  }
+}
 
 // window.addEventListener("scroll",function(){
 //   navBtns.style.marginTop = window.scrollY + "px";
 //   console.log("Scrolling,")
 // })
+//
