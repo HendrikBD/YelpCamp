2569293ab5ce4b9945dc29da25f4ef96cc64fff2
HendrikBD <hendrik.danen@gmail.com>
2018-02-27 15:40:09 -0500

    For campground show navbuttons: styling, sticking while scrolling & fixed jump height

--- public/scripts/show.js
+++ public/scripts/show.js
@@ -1,3 +1,4 @@
+var navBtns = document.querySelector(".navBtns");
 var listItems = document.querySelectorAll(".list-group-item");
 
 listItems[0].addEventListener("click", function(){
@@ -5,8 +6,8 @@ listItems[0].addEventListener("click", function(){
     item.classList.remove("act")
   })
   this.classList.add("act")
-  var jump = document.getElementById("description").offsetTop;
-  window.scrollTo(0, jump);
+  var jump = document.getElementById("activities").offsetTop;
+  window.scrollTo(0, jump+70-window.innerHeight);
 })
 
 listItems[1].addEventListener("click", function(){
@@ -14,8 +15,8 @@ listItems[1].addEventListener("click", function(){
     item.classList.remove("act")
   })
   this.classList.add("act")
-  var jump = document.getElementById("activities").offsetTop;
-  window.scrollTo(0, jump);
+  var jump = document.getElementById("comments").offsetTop;
+  window.scrollTo(0, jump+70-window.innerHeight);
 })
 
 listItems[2].addEventListener("click", function(){
@@ -24,6 +25,10 @@ listItems[2].addEventListener("click", function(){
   })
   this.classList.add("act")
   var jump = document.getElementById("comments").offsetTop;
-  window.scrollTo(0, jump);
+  window.scrollTo(0, jump+70);
 })
 
+// window.addEventListener("scroll",function(){
+//   navBtns.style.marginTop = window.scrollY + "px";
+//   console.log("Scrolling,")
+// })
